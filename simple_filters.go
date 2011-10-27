70933dfdbf1e468b26a4e253495566601e937623
nand chan <nandykins@gmail.com>
2011-10-28 00:23:08 +0200

    Added clamp and scale filters

--- simple_filters.go
+++ simple_filters.go
@@ -1,5 +1,7 @@
 package colorplus
 
+import "math"
+
 // Default filters
 var Identity = FilterSingle(func(in float64) float64 { // returns argument unmodified
     return in
@@ -8,3 +10,25 @@ var Identity = FilterSingle(func(in float64) float64 { // returns argument unmod
 var Invert = FilterSingle(func(in float64) float64 { // inverts an argument on the scale 0-1
     return 1 - in
 })
+
+// Some simple filter providers
+type Clamp struct {
+    Lower, Upper float64
+}
+
+func (cf Clamp) GetSingle() FilterSingle {
+    return func(in float64) float64 {
+        return math.Max(cf.Lower, math.Min(cf.Upper, in))
+    }
+}
+
+// Modify the value range
+type Scale struct {
+    Lower, Upper float64
+}
+
+func (s Scale) GetSingle() FilterSingle {
+    return func(in float64) float64 {
+        return in * (s.Upper - s.Lower) + s.Lower
+    }
+}
